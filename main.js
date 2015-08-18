55bb6ccb90f5d57ebfb7651d17734006304548f8
Jorie Clark <jorie.clark@gmail.com>
2015-08-17 17:09:48 -0700

    pixels

---
+++ main.js
@@ -0,0 +1,37 @@
+var div = document.getElementsByTagName("div");
+var pallete = "padding-bottom:2%; margin: 0; width: 2%; float: left; border: 1px solid #666;bottom:1px;";
+var cssColors = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
+var color;
+
+
+function createBoard(end) {
+   for (var i = 0; i < end; i++) {
+      var newDIV = document.createElement("div");
+      document.body.appendChild(newDIV);
+      div[i].style.cssText = pallete;
+      div[i].style.backgroundColor = cssColors[i];
+   }
+}
+
+function fill() {
+   for (var i = 146; i < div.length; i++) {
+      div[i].addEventListener("click", function () {
+         this.style.backgroundColor = color;
+      });
+   }
+}
+
+function option() {
+   for (var i = 0; i < 146; i++) {
+      div[i].addEventListener("click", function() {
+         color = this.style.backgroundColor; 
+      });
+   }
+}
+window.onload = function () {
+   createBoard(2024);
+   fill();
+   option();
+};
+
+console.log("We made it, guys!");
